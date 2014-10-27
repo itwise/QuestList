08727bec7a6796421bbab032f77c18e2527a2824
kimdongjun <djkim@itwise.co.kr>
2014-10-27 16:18:36 +0900

    init commit

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('todoListApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
